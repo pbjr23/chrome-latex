0cff3520d1140128118797a970ce8ad4cae4dee9
Prakhar Bhandari <prakhar23.bhandari@gmail.com>
2014-09-13 13:40:12 -0400

    Things are looking up in the world

--- background.js
+++ background.js
@@ -1,10 +1,6 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    return {redirectUrl: "http://www.yahoo.com/"};
-  },
-  {
-    urls: [
-      "http://*/*.tex"
-    ]
-  },
-  ["blocking"]);
+chrome.webRequest.onHeadersReceived.addListener(function(details){
+    for(var i = 0; i < details.responseHeaders.length; ++i)
+        if(details.responseHeaders[i].name.toLowerCase() == 'content-type')
+            details.responseHeaders[i].value = 'text/html';
+    return {responseHeaders:details.responseHeaders};
+}, {urls: ["http://*/*.tex"]}, ['blocking', 'responseHeaders']);
