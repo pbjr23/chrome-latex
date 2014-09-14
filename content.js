e30940962220cefb291302a27b75431d6f383b93
David Lakata <dlakata@seas.upenn.edu>
2014-09-13 20:15:18 -0400

    never resizing

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
-var pdf = document.createElement("div");
-pdf.id = "pdf";
-var baseurl = window.location.href.slice(0,-4) + ".pdf";
-var renderedPDF = "http://104.131.63.54:49178/" + baseurl;
-pdf.innerHTML = '<embed id="pdf" src="' + renderedPDF + '" style="position:fixed; top:0; right:0" width="50%" height="100%">';
+var pdfElement = document.createElement("div");
+var baseURL = window.location.href.slice(0,-4) + ".pdf";
+var renderedPDF = "http://104.131.63.54:49178/" + baseURL;
+pdfElement.innerHTML = '<embed src="' + renderedPDF + '" style="position:fixed; top:0; right:0" width="50%" height="100%">';
+document.body.appendChild(pdfElement);
 var latex = document.getElementsByTagName('pre')[0];
-latex.id = "latex";
+
+// CSS styling for LaTeX pane
 latex.style.width = "50%";
 latex.style.float = "left";
-document.body.appendChild(pdf);
+
