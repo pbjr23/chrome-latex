308276d04c903a25e3e2d72b5f259c24026c147f
David Lakata <dlakata@seas.upenn.edu>
2014-09-13 21:50:24 -0400

    iframe fixes default zoom

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 var pdfElement = document.createElement("div");
 var baseURL = window.location.href.slice(0,-4) + ".pdf";
 var renderedPDF = "http://104.131.63.54:49178/" + baseURL;
-pdfElement.innerHTML = '<embed src="' + renderedPDF + '" style="position:fixed; top:0; right:0" width="50%" height="100%">';
+pdfElement.innerHTML = '<iframe src="' + renderedPDF + '" style="position:fixed; top:0; right:0" width="50%" height="100%"></iframe>';
 document.body.appendChild(pdfElement);
 var latex = document.getElementsByTagName('pre')[0];
 
